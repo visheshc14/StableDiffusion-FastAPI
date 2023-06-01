b83a719d967cd4b3dedcb9189fd35e11340b8ae8
Vishesh Choudhary <36515357+visheshc14@users.noreply.github.com>
2023-06-01 17:08:44 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,1 +1,1 @@
-curl -X GET "localhost:8000/api/generate" -H "Content-Type: image/png"
+curl -X GET "localhost:8000/api/generate" -H "Content-Type: image/png" -d '{"image": "https://huggingface.co/front/thumbnails/stable_diffusion.png"}'
